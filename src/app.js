45ea9de32e28d2415e4f3e0f1c0233c581b96e61
Ali Watters <ali.watters@gmail.com>
2017-01-29 06:09:12 -0800

    state at 59:40

--- src/app.js
+++ src/app.js
@@ -1,26 +1,38 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-const source$ = Rx.Observable.interval(200).take(5);
+const source$ = Rx.Observable.interval(1000).take(10).map(v => v * v);
 
-source$.subscribe(
-  x => console.log(x),
-  err => console.log(err),
-  complete => console.log('complete')
-);
+source$.subscribe(x => console.log(x));
 
-const timer$ = Rx.Observable.interval(1000, 200).take(5);
+const names$ = Rx.Observable
+  .from([ 'fred', 'bill', 'alice', 'sally', 'mike' ])
+  .map(v => v.toUpperCase())
+  .map(v => 'I am ' + v);
 
-timer$.subscribe(
-  x => console.log(x),
-  err => console.log(err),
-  complete => console.log('complete')
-);
+names$.subscribe(v => console.log(v));
 
-const range$ = Rx.Observable.range(10, 10);
+function getGithubUser(username) {
+  return $
+    .ajax({
+      url: 'https://api.github.com/users/' + username,
+      dataType: 'jsonp'
+    })
+    .promise();
+}
 
-range$.subscribe(
-  x => console.log(x),
-  err => console.log(err),
-  complete => console.log('complete')
-);
+Rx.Observable
+  .fromPromise(getGithubUser('aliwatters'))
+  .map(user => user.data.name)
+  .subscribe(name => console.log(name));
+
+const users = [
+  { name: 'Arty', age: 44 },
+  { name: 'Joe', age: 14 },
+  { name: 'Emma', age: 31 },
+  { name: 'Zoe', age: 23 }
+];
+
+const users$ = Rx.Observable.from(users);
+
+users$.pluck('name').subscribe(x => console.log(x));
