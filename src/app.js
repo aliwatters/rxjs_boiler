3a1c3c4df7f6594725a85a02af36188011053b7f
Ali Watters <ali.watters@gmail.com>
2017-01-28 21:30:45 -0800

    state at 36:04

--- src/app.js
+++ src/app.js
@@ -1,54 +1,19 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-const numbers = [ 33, 44, 55, 66, 77 ];
+const source$ = new Rx.Observable(observer => {
+  console.log('Creating Observable');
+  observer.next('hello johnny');
+  observer.next('hello mary');
+  observer.error(new Error('ERROR: forced error'));
+  setTimeout(() => observer.next('hello freddy'), 3000);
+  setTimeout(() => observer.complete(), 4000);
+});
 
-const numbers$ = Rx.Observable.from(numbers);
-
-numbers$.subscribe(
-  v => {
-    console.log(v);
-  },
-  err => {
-    console.log('Error', err);
-  },
-  complete => {
-    console.log('Completed');
-  }
-);
-
-const posts = [
-  { title: 'post one', body: 'this is the body' },
-  { title: 'post two', body: 'this is the body' },
-  { title: 'post three', body: 'this is the body' },
-  { title: 'post four', body: 'this is the body' }
-];
-const posts$ = Rx.Observable.from(posts);
-
-posts$.subscribe(
-  v => {
-    console.log(v);
-    $('#posts').append('<li><b>' + v.title + '</b> ' + v.body);
-  },
+source$
+.catch(err => Rx.Observable.of(err))
+.subscribe(
+  x => console.log(x),
   err => console.log(err),
-  complete => console.log('COMPLETED')
-);
-
-const set = new Set(['Hello', 44, {title:'my title'}]);
-const set$ = Rx.Observable.from(set);
-
-set$.subscribe(
-  v => console.log(v),
-  err => console.log(err),
-  complete => console.log('COMPLETED')
-);
-
-
-const map = new Map([[1,2], [3,4], [5,6]]);
-const map$ = Rx.Observable.from(map);
-
-map$.subscribe(
-  v => console.log(v),
-  err => console.log(err),
-  complete => console.log('COMPLETED')
+  complete => console.log('complete')
 );
