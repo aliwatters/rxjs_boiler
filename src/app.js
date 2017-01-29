0d18450dd4316143ed13a9c4ebdc6674fdd8d4d2
Ali Watters <ali.watters@gmail.com>
2017-01-28 21:50:11 -0800

    state at 36:02

--- src/app.js
+++ src/app.js
@@ -3,17 +3,22 @@ import Rx from 'rxjs/Rx';
 
 const source$ = new Rx.Observable(observer => {
   console.log('Creating Observable');
+
   observer.next('hello johnny');
-  observer.next('hello mary');
-  observer.error(new Error('ERROR: forced error'));
+
+  setTimeout(() => observer.next('hello alice'), 1000);
+  // observer.error(new Error('ERROR: forced error'));
   setTimeout(() => observer.next('hello freddy'), 3000);
   setTimeout(() => observer.complete(), 4000);
 });
 
 source$
-.catch(err => Rx.Observable.of(err))
-.subscribe(
-  x => console.log(x),
-  err => console.log(err),
-  complete => console.log('complete')
-);
+  .catch(err => Rx.Observable.of(err))
+  .subscribe(
+    x => {
+      $('#out').html(x);
+      console.log(x);
+    },
+    err => console.log(err),
+    complete => console.log('complete')
+  );
