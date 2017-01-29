793c2b6bd7b7d7f4444108cc436511a73d5f58ed
Ali Watters <ali.watters@gmail.com>
2017-01-28 21:11:11 -0800

    state at 23:40

--- src/app.js
+++ src/app.js
@@ -1,4 +1,52 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+const btn = $('#btn');
+
+const btnStream$ = Rx.Observable.fromEvent(btn, 'click');
+
+btnStream$.subscribe(
+  function(e) {
+    console.log(e.target.innerHTML);
+  },
+  function(err) {
+    console.log('error', err);
+  },
+  function() {
+    console.log('Completed');
+  }
+);
+
+const input = $('#input');
+const output = $('#output');
+
+const inputStream$ = Rx.Observable.fromEvent(input, 'keyup');
+
+inputStream$.subscribe(
+  function(e) {
+    console.log(e.target.value);
+    output.html(e.target.value);
+  },
+  function(err) {
+    console.log('error', err);
+  },
+  function() {
+    console.log('Completed');
+  }
+);
+
+
+const moveStream$ = Rx.Observable.fromEvent(document, 'mousemove');
+
+moveStream$.subscribe(
+  function(e) {
+    console.log(e.target.value);
+    output.html('X:'+e.clientX+ ' Y: '+ e.clientY);
+  },
+  function(err) {
+    console.log('error', err);
+  },
+  function() {
+    console.log('Completed');
+  }
+);
