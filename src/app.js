1bb44bc0d8e058d4747c84fd677222441c60c19e
Ali Watters <ali.watters@gmail.com>
2017-01-28 22:32:52 -0800

    state at 46:29

--- src/app.js
+++ src/app.js
@@ -1,24 +1,36 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-const source$ = new Rx.Observable(observer => {
-  console.log('Creating Observable');
-
-  observer.next('hello johnny');
-
-  setTimeout(() => observer.next('hello alice'), 1000);
-  // observer.error(new Error('ERROR: forced error'));
-  setTimeout(() => observer.next('hello freddy'), 3000);
-  setTimeout(() => observer.complete(), 4000);
+const myPromise = new Promise((resolve, reject) => {
+  console.log('creating promise');
+  setTimeout(() => resolve('Hello from promise'), 3000);
 });
 
-source$
-  .catch(err => Rx.Observable.of(err))
-  .subscribe(
-    x => {
-      $('#out').html(x);
-      console.log(x);
+// myPromise.then(x => console.log(x));
+const source$ = Rx.Observable.fromPromise(myPromise);
+
+source$.subscribe(x => console.log(x));
+
+function getUser(username) {
+  return $
+    .ajax({
+      url: 'https://api.github.com/users/' + username,
+      dataType: 'jsonp'
+    })
+    .promise();
+}
+
+const inputSource$ = Rx.Observable.fromEvent($('#input'), 'keyup');
+
+inputSource$.subscribe(e => {
+  console.log(e.target.value);
+  Rx.Observable.fromPromise(getUser(e.target.value)).subscribe(
+    res => {
+      $('#username').html(res.data.login);
+      $('#name').html(res.data.name);
+      $('#url').html(res.data.url);
     },
     err => console.log(err),
     complete => console.log('complete')
   );
+});
